955ca5defd9bb02eac6c941a379eb5cbe6f9b71f
Manisha Dhyani <dhyanimanishals95@gmail.com>
2025-07-16 13:46:55 +0530

    basic structure for SB project

---
+++ src/main/java/com/springframework/ranga/learn_springframework/LearnSpringframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.springframework.ranga.learn_springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LearnSpringframeworkApplication.class, args);
+	}
+
+}
